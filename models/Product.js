d48f446b1878a6a0f5c5a84d7239f89f51a62aa8
Anis Sarker <shahidsarker95@hotmail.com>
2018-03-21 03:40:36 +0000

    assignment 13

---
+++ models/Product.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ProductSchema = {
+
+  title: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Title required'
+  },
+
+  description: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'A short description required'
+  },
+
+  price: {
+    type: Number,
+    trim: true,
+    required: 'Price required'
+  },
+  
+  quantity: {
+    type: Number,
+    trim: true,
+    required: 'Quantity required'
+  },
+  
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+
+  created: {
+    type: Date,
+    default: Date.now
+  }
+}
+
+var Product = mongoose.model('Product', ProductSchema, 'products');
+module.exports = Product;
